7a8a983bb69c60d4373048b87afe82355ffa156f
mayusuf <samba.yusuf@gmail.com>
2024-10-04 09:30:18 -0500

    added

---
+++ Lab4-Inheritance/src/main/java/org/example/lab4inheritance/Lab4InheritanceApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab4inheritance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4InheritanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4InheritanceApplication.class, args);
+    }
+
+}
---
+++ Lab4-Inheritance/src/test/java/org/example/lab4inheritance/Lab4InheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab4inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4InheritanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
